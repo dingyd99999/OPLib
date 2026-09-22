ea71169a3082372a7f1bc6d26be866f08ca4e024
dingyd99999 <dingyd99999@163.com>
2026-09-22 17:37:08 +0800

    Add files via upload

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+
+        public double Divide(double dividend, double divisor)
+        {
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException("除数不能为 0。");
+            }
+
+            return dividend / divisor;
+        }
+
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+    }
+}
